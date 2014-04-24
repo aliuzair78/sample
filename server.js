62f3e65b0a62ace1c4fea5b092a401061fe597f2
aliuzair78 <11011556-048@uog.edu.pk>
2014-04-24 08:58:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-var http = require("http");
-var server = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<!DOCTYPE "html">");
-  response.write("<html>");
-  response.write("<head>");
-  response.write("<title>Hello World Page</title>");
-  response.write("</head>");
-  response.write("<body>");
-  response.write("Hello World!");
-  response.write("</body>");
-  response.write("</html>");
-  response.end();
-});
- 
-server.listen(80);
-console.log("Server is listening");
+//  OpenShift sample Node application
+var http = require('http');
+
+//Get the environment variables we need.
+var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port    = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+http.createServer(function (req, res) {
+	var addr = "unknown";
+	var out = "";
+	if (req.headers.hasOwnProperty('x-forwarded-for')) {
+		addr = req.headers['x-forwarded-for'];
+	} else if (req.headers.hasOwnProperty('remote-addr')){
+		addr = req.headers['remote-addr'];
+	}
+
+	if (req.headers.hasOwnProperty('accept')) {
+		if (req.headers['accept'].toLowerCase() == "application/json") {
+			  res.writeHead(200, {'Content-Type': 'application/json'});
+			  res.end(JSON.stringify({'ip': addr}, null, 4) + "\n");			
+			  return ;
+		}
+	}
+
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write("Welcome to Node.js on OpenShift!\n\n");
+  res.end("Your IP address seems to be " + addr + "\n");
+}).listen(port, ipaddr);
+console.log("Server running at http://" + ipaddr + ":" + port + "/");
